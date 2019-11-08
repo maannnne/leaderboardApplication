f47c7fb7eec926119ab8cb0f00320ecca1483fac
maannnne <mane.poghosian@gmail.com>
2019-11-08 17:34:38 +0400

    Add function to increment players score

--- leaderboard/client/main.js
+++ leaderboard/client/main.js
@@ -5,11 +5,7 @@ import { Template } from 'meteor/templating';
 import './main.html';
 //new Mongo.Collection('players');
 PlayersList = new Mongo.Collection('players');
-// PlayersList.insert({name: "David", score: 0});
-// PlayersList.insert({name: "Bob", score: 0});
-// PlayersList.insert({name: "Amy", score: 0});
-
-
+//PlayersList.insert({name: "David", score: 0});
 
 if(Meteor.isClient){
   console.log("Hello Client");
@@ -30,17 +26,13 @@ Template.leaderboard.helpers({
 
 
 Template.leaderboard.events({
-    // 'click .player': (e) => {  
-    //   var playersID = e.target.id; //playersID-in talis enq targeted player i unique ID-n
-    //   Session.set('selectedPlayer', playersID); //session enq sarqum selectedPlayer anunov u iran talis enq playersID-i arjeqy
-    //   var selectedPlayer = Session.get('selectedPlayer'); //session-ic get enq anum ed arjeqy u talis enq selectedPlayer var-in 
-    //   console.log(selectedPlayer); //output enq anum iran console um
-    // }
     'click .player': function() {
       var playersID = this._id;
       Session.set('selectedPlayer', playersID);
     },
     'click .increment': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update(selectedPlayer, {$inc: {score: 1}});
 
     }
     //DO NOT USE ARROW FUNTIONS WHEN U NEED TO USE THIS KEYWORD
