dd7b927645274bc9b860589549d1a7239f406215
maannnne <mane.poghosian@gmail.com>
2019-11-08 16:52:25 +0400

    A few changes w functions

--- leaderboard/client/main.js
+++ leaderboard/client/main.js
@@ -1,12 +1,14 @@
+import { Session } from 'meteor/session';
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { Template } from 'meteor/templating';
+import './main.html';
 //new Mongo.Collection('players');
 PlayersList = new Mongo.Collection('players');
 // PlayersList.insert({name: "David", score: 0});
 // PlayersList.insert({name: "Bob", score: 0});
-// PlayersList.insert({name: "Betty", score: 0});
+// PlayersList.insert({name: "Amy", score: 0});
 
-import { Meteor } from 'meteor/meteor';
-import { Template } from 'meteor/templating';
-import './main.html';
 
 
 if(Meteor.isClient){
@@ -15,14 +17,33 @@ if(Meteor.isClient){
 
 Template.leaderboard.helpers({
   'getPlayer': () => {
-    return PlayersList.find();
+    return PlayersList.find().fetch();
+  },
+  'selectedClassHelper': function() {
+    var playersID = this._id;
+    var selectedPlayer = Session.get('selectedPlayer');
+    if(playersID == selectedPlayer){
+      return "selected"; //this is the class name
+    }
   }
 });
 
 
 Template.leaderboard.events({
-    'click .player': () => {
-      Session.set('selectedPlayer', )
-    }
+    // 'click .player': (e) => {  
+    //   var playersID = e.target.id; //playersID-in talis enq targeted player i unique ID-n
+    //   Session.set('selectedPlayer', playersID); //session enq sarqum selectedPlayer anunov u iran talis enq playersID-i arjeqy
+    //   var selectedPlayer = Session.get('selectedPlayer'); //session-ic get enq anum ed arjeqy u talis enq selectedPlayer var-in 
+    //   console.log(selectedPlayer); //output enq anum iran console um
+    // }
+    'click .player': function() {
+      var playersID = this._id;
+      Session.set('selectedPlayer', playersID);
+    },
+    'click .increment': function(){
 
-})
+    }
+    //DO NOT USE ARROW FUNTIONS WHEN U NEED TO USE THIS KEYWORD
+    //BASED ON CONTEXT IN WHICH IT IS CALLED !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+});
